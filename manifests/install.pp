eec897c757f62369cc8cd6f2039689d4f1bf7292
Bram Vogelaar <bram@attachmentgenie.com>
2020-06-15 23:53:54 +0200

    initial work related to logcli

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,9 +8,12 @@ class loki::install {
   case $::loki::install_method {
     'archive': {
       $release_file_name = 'loki-linux-amd64'
+      $release_file_name_logcli = 'loki-linux-amd64'
+
       $version_dir = "${loki::data_dir}/loki-${loki::version}"
 
       $binary_path = "${version_dir}/${release_file_name}"
+      $binary_path_logcli = "${version_dir}/${release_file_name_logcli}"
 
       if $::loki::manage_user {
         user { 'loki':
@@ -43,6 +46,16 @@ class loki::install {
         user         => $::loki::user,
         group        => $::loki::group,
       }
+      -> archive { "${binary_path_logcli}.zip":
+        ensure       => present,
+        source       => "https://github.com/grafana/loki/releases/download/${loki::version}/${release_file_name_logcli}.zip",
+        extract      => true,
+        extract_path => $version_dir,
+        creates      => $binary_path_logcli,
+        cleanup      => false,
+        user         => $::loki::user,
+        group        => $::loki::group,
+      }
 
       file {
         $binary_path:
@@ -54,6 +67,20 @@ class loki::install {
           ensure  => link,
           target  => $binary_path,
           require => File[$binary_path],
+          notify  => Service['loki'],
+        ;
+      }
+
+      file {
+        $binary_path_logcli:
+          ensure  => file,
+          mode    => '0755',
+          require => Archive["${binary_path_logcli}.zip"],
+        ;
+        "${loki::bin_dir}/logcli":
+          ensure  => link,
+          target  => $binary_path_logcli,
+          require => File[$binary_path_logcli],
           notify  => Service['loki'],
         ;
       }
