3a2ac65567f5615c71e6a58bbeb4464aecfa3fe5
Bram Vogelaar <bram@attachmentgenie.com>
2021-08-30 21:45:15 +0200

    adding minimal required setup

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -4,7 +4,13 @@ require 'spec_helper_acceptance'
 
 describe 'with default parameters ', if: ['debian', 'redhat', 'ubuntu'].include?(os[:family]) do
   pp = <<-PUPPETCODE
-  class { '::loki': }
+  class{ 'loki':
+    auth_enabled                => false,
+    schema_config_hash          => {'schema_config' => {'configs' => [{'from' => '2020-05-15', 'store' => 'boltdb', 'object_store' => 'filesystem', 'schema' => 'v11', 'index' =>{'prefix' => 'index_', 'period' => '168h'}}]}},
+    storage_config_hash         => {'storage_config' => { 'boltdb' => { 'directory' => '/var/lib/loki/index',}, 'filesystem' => {'directory' => '/var/lib/loki/chunks',},},},
+    server_config_hash          => {'server' => {'http_listen_port' => 3100, 'http_listen_address' => $facts['networking']['ip']},},
+    ingester_client_config_hash => {'ingester' => { 'lifecycler' => {'interface_names' => [$facts['networking']['primary']], 'address' => '127.0.0.1', 'ring' =>{'kvstore' =>{'store' => 'inmemory'}, 'replication_factor' => 1}}}},
+  }
 PUPPETCODE
 
   it 'applies idempotently' do
